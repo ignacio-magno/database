3db17d054be343c1fe5f0283d984dfbab41cb788
ignacioMagno <lasterriens@gmail.com>
2022-08-24 13:12:48 -0400

    with save or replace

---
+++ saveOrReplace/main.go
@@ -0,0 +1,48 @@
+package saveorreplace
+
+import (
+	"github.com/ignacioMagno/database"
+)
+
+type SaveOrReplace interface {
+	GetDatabase() database.Db
+	GetFilter() interface{}
+	GetObject() interface{}
+	IsNew() bool
+}
+
+func Save(s SaveOrReplace) error {
+	if s.IsNew() {
+		return saveNew(s)
+	} else {
+		return replace(s)
+	}
+}
+
+func saveNew(s SaveOrReplace) error {
+	db := s.GetDatabase()
+
+	save := database.Save{
+		Db: db,
+		Add: database.Add{
+			Data: s.GetObject(),
+		},
+	}
+
+	_, err := save.SaveOne()
+	return err
+
+}
+
+func replace(s SaveOrReplace) error {
+	db := s.GetDatabase()
+
+	db.Filter = s.GetFilter()
+
+	save := database.FindAndModify{
+		Db:   db,
+		Data: s.GetObject(),
+	}
+
+	return save.FindOneAndReplace()
+}
